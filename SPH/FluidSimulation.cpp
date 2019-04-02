d161517c2b6b65ae47a56328446d8e3d2c8ef988
Thijs Voncken <tjvoncken@gmail.com>
2019-04-02 13:09:08 +0200

    Added basic gravity to the fluid sim.

--- SPH/FluidSimulation.cpp
+++ SPH/FluidSimulation.cpp
@@ -3,7 +3,14 @@
 #include "./ParticleCollection.h"
 
 namespace SPH {
+	FluidSimulation::FluidSimulation(Eigen::Vector3d gravity): gravity(gravity) {}
+
 	void FluidSimulation::update(const double deltaTime, ParticleCollection& particles) {
+		// Update velocity with gravity.
+		particles.velocityX.array() += deltaTime * this->gravity[0];
+		particles.velocityY.array() += deltaTime * this->gravity[1];
+		particles.velocityZ.array() += deltaTime * this->gravity[2];
+
 		// Update positions by integrating velocities.
 		particles.positionX += deltaTime * particles.velocityX;
 		particles.positionY += deltaTime * particles.velocityY;
--- SPH/FluidSimulation.h
+++ SPH/FluidSimulation.h
@@ -2,6 +2,10 @@
 
 namespace SPH {
 	class FluidSimulation {
+		Eigen::Vector3d gravity; // Gravity as acceleration.
+
+		FluidSimulation(Eigen::Vector3d gravity);
+
 		void update(const double deltaTime, ParticleCollection& particles);
 	};
 }
