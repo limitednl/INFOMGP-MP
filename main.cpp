fc1aa2de230b6c56af76485bf0b8898ac1da2b1e
Floris Schild <Floris@2at.nl>
2019-04-02 13:07:58 +0200

    Added "sphere" meshes

--- main.cpp
+++ main.cpp
@@ -46,13 +46,57 @@ bool key_down(igl::opengl::glfw::Viewer& viewer, unsigned char key, int modifier
 	return false;
 }
 
+void GetSphereMesh(double radius, MatrixXd& vertices, MatrixXi& faces) {
+	vertices.resize(8, 3);
+	vertices <<
+		-radius, radius, -radius,
+		-radius, radius, radius,
+		radius, radius, radius,
+		radius, radius, -radius,
+		-radius, -radius, -radius,
+		-radius, -radius, radius,
+		radius, -radius, radius,
+		radius, -radius, -radius;
+
+	faces.resize(12, 3);
+	faces <<
+		0, 1, 2,
+		2, 3, 0,
+		6, 5, 4,
+		4, 7, 6,
+		1, 0, 5,
+		0, 4, 5,
+		2, 1, 6,
+		1, 5, 6,
+		3, 2, 7,
+		2, 6, 7,
+		0, 3, 4,
+		3, 7, 4;
+}
+
 bool pre_draw(igl::opengl::glfw::Viewer& viewer)
 {
 	using namespace Eigen;
 	using namespace std;
 
 	if (viewer.core.is_animating) {
-		//scene.updateScene();
+		RowVector3d color; color << 0.8, 0.1, 0.1; //red-ish?
+
+		//todo: set;
+		MatrixXd vertices;
+		MatrixXi faces;
+
+		//x,y,z,radius
+		Vector4d particle; particle << 0, 0, 0, 20;
+
+ 		GetSphereMesh(1, vertices, faces);
+
+		viewer.data_list[0].clear();
+		viewer.data_list[0].set_mesh(vertices, faces);
+		viewer.data_list[0].set_face_based(true);
+		viewer.data_list[0].show_lines = false;
+		viewer.data_list[0].set_colors(color);
+		viewer.core.align_camera_center(vertices);
 	}
 
 	return false;
@@ -79,6 +123,7 @@ int main(int argc, char* argv[])
 	igl::opengl::glfw::imgui::ImGuiMenu menu;
 	viewer.plugins.push_back(&menu);
 	viewer.callback_key_down = &key_down;
+	viewer.callback_pre_draw = &pre_draw;
 
 	while (relaunch)
 	{
@@ -89,3 +134,5 @@ int main(int argc, char* argv[])
 		viewer.launch(!fScreen, fScreen);
 	}
 }
+
+
